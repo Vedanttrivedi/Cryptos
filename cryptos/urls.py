4c3814a556ce3da09556125f1c3825fec2cc4f32
Vedanttrivedi <53808109+Vedanttrivedi@users.noreply.github.com>
2021-12-10 11:24:00 +0530

    login,registration,login with google,forget password backend added

---
+++ cryptos/urls.py
@@ -0,0 +1,31 @@
+"""cryptos URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from user import views as user_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('',user_views.home,name="home page"),
+    path('accounts/', include('allauth.urls')),
+    path('admin/', admin.site.urls),
+    path('user/',include('user.urls')),
+    path('password-reset/',
+    user_views.password_reset_request,name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="user/password/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password/password_reset_complete.html'), name='password_reset_complete'),     
+]
---
+++ user/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render, resolve_url,redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Welcome Home")
+    
+def login(request):
+    if request.method=="POST":
+        name = request.POST["username"]
+        password =make_password( request.POST["password"])
+        user = User.objects.filter(username=name,password=password)
+        user1 =  User.objects.filter(email=name,password=password)
+        if  user is not None:
+            login(request,user)
+            return HttpResponse(f"{user.username}")
+        elif  user1 is not None:
+            login(request,user1)
+            return HttpResponse(f"{user1.username}")
+        if True:
+            return redirect("/user/login")
+    return render(request,"user/login.html")
+
+
+
+def register(request):
+    if request.method=="POST":
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = make_password(request.POST["password"])
+        conpassword = make_password(request.POST["conpassword"])
+        user=  User(first_name=firstname,last_name=lastname,email=email,password=password,username=username)
+        user.is_active = True
+        user.save()
+        return redirect("/user/login")
+    return render(request,"user/register.html")
+
+
+
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			associated_users = User.objects.filter(Q(email=data))
+			if associated_users.exists():
+				for user in associated_users:
+					subject = "Password Reset Requested"
+					email_template_name = "user/password/password_reset_email.txt"
+					c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8000',
+					'site_name': 'Cryptos',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+					email = render_to_string(email_template_name, c)
+					try:
+						send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+					except BadHeaderError:
+						return HttpResponse('Invalid header found.')
+					return redirect ("/password_reset/done/")
+	password_reset_form = PasswordResetForm()
+	return render(request=request, template_name="user/password/password_reset.html", context={"password_reset_form":password_reset_form})
